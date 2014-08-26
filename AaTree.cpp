37e766db41c59e999d7cecffd29c70d715a6cb9c
no-glue <nikolapav1985@gmail.com>
2014-08-26 22:01:41 +0200

    export tree find

--- AaTree.cpp
+++ AaTree.cpp
@@ -3,8 +3,11 @@
 
 using namespace boost::python;
 
-void (AaTree::*insert)(string, string) = &AaTree::insert;
+void (AaTree::*insert_key)(string, string) = &AaTree::insert;
+void (AaTree::*remove_key)(string) = &AaTree::remove;
+AaNode* (AaTree::*find_key)(string) = &AaTree::find;
+int (AaTree::*level)() = &AaTree::level;
 
 BOOST_PYTHON_MODULE(AaDictionary) {
-  class_<AaTree>("AaTree").def("insert", insert);
+  class_<AaTree>("AaTree").def("insert_key", insert_key).def("remove", remove_key).def("find_key", find_key).def("level", level);
 };
