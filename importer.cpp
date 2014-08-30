1f18e263be5c91f4950378594db48b7d4202bab2
no-glue <nikolapav1985@gmail.com>
2014-08-30 11:30:29 +0200

    templated metrics

--- importer.cpp
+++ importer.cpp
@@ -1,4 +1,5 @@
 #include "importer.h"
+#include "metrics.h"
 #include "decorator_average_path_length.h"
 #include "decorator_bfs_message.h"
 #include "decorator_not_seen.h"
@@ -7,29 +8,31 @@
 int main() {
   AaTree * tree = new AaTree();
   Importer * importer = new Importer();
+  Metrics<AaNode, AaTree> * metrics = new Metrics<AaNode, AaTree>();
   DecoratorAveragePathLength * av_path_len = new DecoratorAveragePathLength();
   DecoratorBfsMessage * bfs_message = new DecoratorBfsMessage();
   DecoratorNotSeen * not_seen = new DecoratorNotSeen();
   DecoratorMaxDepth * get_max_depth = new DecoratorMaxDepth();
   importer->import("../../node/netmark/data_live_journal/split/", tree);
   // todo use first data set
-  cout<<"nodes: "<<importer->nodes(tree)<<endl;
+  cout<<"nodes: "<<metrics->nodes(tree)<<endl;
   // todo move this to component
-  cout<<"edges: "<<importer->edges(tree)<<endl;
+  cout<<"edges: "<<metrics->edges(tree)<<endl;
   // todo move this to component
-  cout<<"density: "<<importer->density(tree)<<endl;
+  cout<<"density: "<<metrics->density(tree)<<endl;
   // todo move this to component
-  cout<<"average degree: "<<importer->average_degree(tree)<<endl;
+  cout<<"average degree: "<<metrics->average_degree(tree)<<endl;
   // todo move this to component
-  cout<<"average path length: "<<importer->breadth_first_search<AaNode, queue<string>, queue<int>, AaTree, DecoratorAveragePathLength, DecoratorBfsMessage>(tree, av_path_len, bfs_message)<<endl;
+  cout<<"average path length: "<<metrics->breadth_first_search<queue<string>, queue<int>, DecoratorAveragePathLength, DecoratorBfsMessage>(tree, av_path_len, bfs_message)<<endl;
   // todo move this to component
   tree->walk(not_seen);
-  cout<<"network diameter: "<<importer->breadth_first_search<AaNode, queue<string>, queue<int>, AaTree, DecoratorMaxDepth, DecoratorBfsMessage>(tree, get_max_depth, bfs_message)<<endl;
+  cout<<"network diameter: "<<metrics->breadth_first_search<queue<string>, queue<int>, DecoratorMaxDepth, DecoratorBfsMessage>(tree, get_max_depth, bfs_message)<<endl;
   // todo move this to component
   delete get_max_depth;
   delete not_seen;
   delete bfs_message;
   delete av_path_len;
+  delete metrics;
   delete importer;
   delete tree;
   return 0;
