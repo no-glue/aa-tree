4f95a3f1d97c4bd198ff563508925292a2d2159b
no-glue <nikolapav1985@gmail.com>
2014-08-31 01:48:50 +0200

    updated test tree

--- test_tree.cpp
+++ test_tree.cpp
@@ -1,16 +1,30 @@
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <assert.h>
+#include <dirent.h>
+#include <queue>
+#include <stack>
+#include "aa_node.h"
+#include "aa_tree.h"
 #include "test_tree.h"
 
+using namespace std;
+
 int main() {
-  TestTree * tree_test = new TestTree();
-  AaTree * tree = new AaTree();
+  TestTree<AaTree<AaNode<string, vector<string> >, string>, AaNode<string, vector<string> >, string> * tree_test = new TestTree<AaTree<AaNode<string, vector<string> >, string>, AaNode<string, vector<string> >, string>();
+  AaTree<AaNode<string, vector<string> >, string> * tree = new AaTree<AaNode<string, vector<string> >, string>();
   string key1, value1, key2, value2;
   key1 = "like"; value1 = "icecream";
   tree_test->test_insert_key(key1, value1, tree);
-  tree = new AaTree(); key1 = "like"; value1 = "icecream"; key2 = "like"; value2 = "chocolate";
+  tree = new AaTree<AaNode<string, vector<string> >, string>(); key1 = "like"; value1 = "icecream"; key2 = "like"; value2 = "chocolate";
   tree_test->test_insert_keys(key1, value1, key2, value2, tree);
-  tree = new AaTree(); key1 = "like"; value1 = "icecream";
+  tree = new AaTree<AaNode<string, vector<string> >, string>(); key1 = "like"; value1 = "icecream";
   tree_test->test_remove_key(key1, value1, tree);
-  tree = new AaTree(); key1 = "like"; value1 = "icecream"; key2 = "like"; value2 = "chocolate";
+  tree = new AaTree<AaNode<string, vector<string> >, string>(); key1 = "like"; value1 = "icecream"; key2 = "like"; value2 = "chocolate";
   tree_test->test_level(key1, value1, key2, value2, tree);
   delete tree_test;
 }
--- test_tree.h
+++ test_tree.h
@@ -1,35 +1,33 @@
-#include "aa_tree.h"
-
-class TestTree {
+template<class Tree, class Node, typename Str>class TestTree {
 public:
-  void test_insert_key(string key, string value, AaTree * & tree) {
+  void test_insert_key(Str key, Str value, Tree * & tree) {
     // test single key insert
-    AaNode * found;
+    Node * found;
     tree->insert(key, value);
     found = tree->find(key);
     assert(found->value[0] == value);
     delete tree;
   }
-  void test_insert_keys(string key1, string value1, string key2, string value2, AaTree * & tree) {
+  void test_insert_keys(Str key1, Str value1, Str key2, Str value2, Tree * & tree) {
     // test same key, different values
-    AaNode * found;
+    Node * found;
     tree->insert(key1, value1);
     tree->insert(key2, value2);
     found = tree->find(key1);
     assert(found->value[0] == value1);
     delete tree;
   }
-  void test_remove_key(string key, string value, AaTree * & tree) {
+  void test_remove_key(Str key, Str value, Tree * & tree) {
     // test remove key
     // should not be there after remove
-    AaNode * found;
+    Node * found;
     tree->insert(key, value);
     tree->remove(key);
     found = tree->find(key);
     assert(found == 0);
     delete tree;
   }
-  void test_level(string key1, string value1, string key2, string value2, AaTree * & tree) {
+  void test_level(Str key1, Str value1, Str key2, Str value2, Tree * & tree) {
     // test level
     // should be 1 for 2 keys
     tree->insert(key1, value1);
