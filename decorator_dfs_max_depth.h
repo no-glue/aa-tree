730aa15d646fcf66c175df780eb2d12a1933dc88
no-glue <nikolapav1985@gmail.com>
2014-08-30 02:09:37 +0200

    removed dfs

--- decorator_dfs_max_depth.h
+++
@@ -1,7 +0,0 @@
-class DecoratorDfsMaxDepth {
-public:
-  float return_is(float ret, int edges, int length, int current_depth) {
-    // just return current depth
-    return current_depth;
-  }
-};
--- decorator_dfs_message.h
+++
@@ -1,6 +0,0 @@
-class DecoratorDfsMessage {
-public:
-  void message(int edges, int stack_size) {
-    if(!(edges % 1000000)) cout<<"Edges: "<<edges<<" Size: "<<stack_size<<endl;
-  }
-};
