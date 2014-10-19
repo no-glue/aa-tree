5e0fdcac6c59240c867efc3ef864f615fbdc5c48
no-glue <nikola_pav@eunet.rs>
2014-10-19 17:55:07 +0200

    removed bfs decorators

--- decorator_average_path_length.h
+++
@@ -1,9 +0,0 @@
-class DecoratorAveragePathLength {
-public:
-  float return_is(float ret, int & edges, int & length, int & start_depth, int & current_depth) {
-    // find average path length
-    edges++;
-    length+=start_depth;
-    return length / (float)edges;
-  }
-};
--- decorator_not_seen.h
+++
@@ -1,7 +0,0 @@
-class DecoratorNotSeen {
-public:
-  template<typename Node> void visit(Node * & root, bool flag = false) {
-    // clear visited for root
-    root->visited = flag;
-  }
-};
