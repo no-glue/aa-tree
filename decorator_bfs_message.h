8000d0035a8d97333aec1c4ae22e13389f66b50e
no-glue <nikola_pav@eunet.rs>
2014-10-19 15:57:24 +0200

    removed bfs decorators

--- decorator_bfs_message.h
+++
@@ -1,8 +0,0 @@
-template<class Ostream>class DecoratorBfsMessage {
-public:
-  Ostream & stream;
-  DecoratorBfsMessage(Ostream & stream):stream(stream) {}
-  void message(float ret, int edges, int length) {
-    if(edges && !(edges % 1000000)) stream<<"Edges: "<<edges<<"\n";
-  }
-};
--- decorator_max_depth.h
+++
@@ -1,8 +0,0 @@
-class DecoratorMaxDepth {
-public:
-  float return_is(float ret, int & edges, int & length, int & start_depth, int & current_depth) {
-    if(current_depth < start_depth) current_depth = start_depth;
-
-    return current_depth;
-  }
-};
